a0de51a226939f321b2a66144a634bd17f9f2a54
arielrecto <arielrecto@gmail.com>
2023-05-15 23:02:16 +0800

    Fix bugs, change profile route to user route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,11 +11,14 @@
                 </div>
 
                 <!-- Navigation Links -->
+
+                @if(Auth::user())
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
